c5a0ed4beece182fd6e34f65cee6d7ed5043117b
ssc <1033729276@qq.com>
2019-07-22 21:47:59 +0800

    feat:avatar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,17 @@
 			<SlideMenu></SlideMenu>
 	  </el-aside>
 	  <el-container>
-	    <el-header>Header
-
+	    <el-header>
+				<el-row class="demo-avatar demo-basic">
+					<el-col :span="12">
+					  <div class="sub-title">circle</div>
+					  <div class="demo-basic--circle">
+					    <div class="block">
+					      <el-avatar :size="size" :src="circleUrl"></el-avatar>
+					    </div>
+					  </div>
+					</el-col>
+				</el-row>
 	    </el-header>
 	    <el-main>main
 	    	<router-view/>
@@ -43,6 +52,12 @@
 import SlideMenu from "@/components/SlideMenu.vue"
 export default {
   name: 'home',
+  data(){
+  	return{
+  		circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+  		sizeList: "large"
+  	}
+  },
   components: {
 		SlideMenu
   }
